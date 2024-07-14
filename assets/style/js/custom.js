443334f29b480bcef78f560e13fd85a2a6929a41
Ipsusoedra <iipsudrajat@gmail.com>
2024-07-14 11:03:56 +0700

    first commit

---
+++ assets/style/js/custom.js
@@ -0,0 +1,157 @@
+var sarana = new Swiper(".slide-sarana", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".sarana-button-next",
+    prevEl: ".sarana-button-prev",
+  },
+  pagination: {
+    el: ".sarana-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+var kegiatan = new Swiper(".slide-kegiatan", {
+  cssMode: true,
+  slidesPerView: 2.5,
+  spaceBetween: 0,
+  breakpoints: {
+    300: {
+      slidesPerView: 2,
+      spaceBetween: -30,
+    },
+    400: {
+      slidesPerView: 3,
+      spaceBetween: -30,
+    },
+    500: {
+      slidesPerView: 3.9,
+    },
+    600: {
+      slidesPerView: 2.3,
+    },
+    700: {
+      slidesPerView: 2.8,
+    },
+    768: {
+      slidesPerView: 2.5,
+    },
+    992: {
+      slidesPerView: 2.5,
+    },
+    1200: {
+      slidesPerView: 2.8,
+    },
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+var prestasiNasional = new Swiper(".slide-prestasi-nasional", {
+  cssMode: true,
+  slidesPerView: 2.5,
+  spaceBetween: 0,
+  breakpoints: {
+    300: {
+      slidesPerView: 2,
+      spaceBetween: -30,
+    },
+    400: {
+      slidesPerView: 3,
+      spaceBetween: -30,
+    },
+    500: {
+      slidesPerView: 3.9,
+    },
+    600: {
+      slidesPerView: 2.3,
+    },
+    700: {
+      slidesPerView: 2.8,
+    },
+    768: {
+      slidesPerView: 2.5,
+    },
+    992: {
+      slidesPerView: 2.5,
+    },
+    1200: {
+      slidesPerView: 2.8,
+    },
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+var prestasiProvinsi = new Swiper(".slide-prestasi-provinsi", {
+  cssMode: true,
+  slidesPerView: 2.5,
+  spaceBetween: 0,
+  breakpoints: {
+    300: {
+      slidesPerView: 2,
+      spaceBetween: -30,
+    },
+    400: {
+      slidesPerView: 3,
+      spaceBetween: -30,
+    },
+    500: {
+      slidesPerView: 3.9,
+    },
+    600: {
+      slidesPerView: 2.3,
+    },
+    700: {
+      slidesPerView: 2.8,
+    },
+    768: {
+      slidesPerView: 2.5,
+    },
+    992: {
+      slidesPerView: 2.5,
+    },
+    1200: {
+      slidesPerView: 2.8,
+    },
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+var prestasiDaerah = new Swiper(".slide-prestasi-daerah", {
+  cssMode: true,
+  slidesPerView: 2.5,
+  spaceBetween: 0,
+  breakpoints: {
+    300: {
+      slidesPerView: 2,
+      spaceBetween: -30,
+    },
+    400: {
+      slidesPerView: 3,
+      spaceBetween: -30,
+    },
+    500: {
+      slidesPerView: 3.9,
+    },
+    600: {
+      slidesPerView: 2.3,
+    },
+    700: {
+      slidesPerView: 2.8,
+    },
+    768: {
+      slidesPerView: 2.5,
+    },
+    992: {
+      slidesPerView: 2.5,
+    },
+    1200: {
+      slidesPerView: 2.8,
+    },
+  },
+  mousewheel: true,
+  keyboard: true,
+});
